0e1096fff67e55e9e87c930f1230d69e1b1c09ef
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-04 10:24:51 +0500

    Task2

--- Tyuiu.DanilovAS.Sprint3.Task1.V21.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint3.Task1.V21.Lib/DataService.cs
@@ -5,14 +5,15 @@ namespace Tyuiu.DanilovAS.Sprint3.Task1.V21.Lib
     {
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
-            double p = 1;
+            double p = 1.0;
+
             while (startValue <= stopValue)
             {
-                p *= Math.Pow((0.5 + startValue / 5.0) / (Math.Cos(value) + 0.5), 3);
+                p = p * Math.Pow((0.5 + startValue/5.0)/(Math.Cos(value)+0.5), 3);
                 startValue++;
             }
 
-            return Math.Round(p, 3);
+            return Math.Round(p,3);
         }
     }
 }
---
+++ Tyuiu.DanilovAS.Sprint3.Task2.V27/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.DanilovAS.Sprint3.Task2.V27.Lib;
+namespace Tyuiu.DanilovAS.Sprint3.Task2.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+            Console.WriteLine("* ряда по формуле, при x = 5                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            Console.WriteLine($"Ответ равен => {res}");
+            Console.ReadKey();
+
+        }
+    }
+}
