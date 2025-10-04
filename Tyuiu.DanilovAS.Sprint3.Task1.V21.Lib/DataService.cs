d01510cd81b05c7fe048f9922a58c17e0ed7481d
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-04 09:27:54 +0500

    Task1

--- Tyuiu.DanilovAS.Sprint3.Task1.V21.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint3.Task1.V21.Lib/DataService.cs
@@ -6,7 +6,6 @@ namespace Tyuiu.DanilovAS.Sprint3.Task1.V21.Lib
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
             double p = 1;
-
             while (startValue < stopValue)
             {
                 p = Math.Pow(0.5 + (value / (5)) / (Math.Cos(value) + 0.5),3);
