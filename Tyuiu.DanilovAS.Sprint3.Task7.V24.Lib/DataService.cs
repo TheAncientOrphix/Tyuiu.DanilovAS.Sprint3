d193dfcf92ff92a7b8f458a9dfa284001b92cffe
Александр Данилов <Tyuiu.DanilovAS@outlook.com>
2025-10-06 11:33:08 +0500

    Task7

---
+++ Tyuiu.DanilovAS.Sprint3.Task7.V24.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System.Transactions;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.DanilovAS.Sprint3.Task7.V24.Lib
+{
+    public class DataService : ISprint3Task7V24
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] result;
+            int len = (stopValue - startValue) + 1;
+            result = new double[len];
+            double y;
+            int count = 0;
+            for(int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Sin(x) / (x + 1.2) - Math.Sin(x) * 2 - 2 * x;
+                result[count] = Math.Round(y,2);
+                count++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint3.Task7.V24/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Serialization.Metadata;
+using Tyuiu.DanilovAS.Sprint3.Task7.V24.Lib;
+namespace Tyuiu.DanilovAS.Sprint3.Task7.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+            Console.WriteLine("* F(x) = sin(x)/(x+1.2) - sin(x) *2 - 2*x на заданном диапазоне [-5; 5]   *");
+            Console.WriteLine("* с шагом 1. Произвести проверку деления на ноль. При деление на ноль     *");
+            Console.WriteLine("* вернуть значение 0. Значение округлить до двух знаков после запятой     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] res;
+            res = new double[len];
+
+            res = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     | {1,5:f2} |", startValue, res[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+
+
+        }
+    }
+}
